eb4fd24eaed4cc1c3ea991bd31743dc99f967d7f
shenan2001 <85660117+ShenanPerera@users.noreply.github.com>
2022-07-30 06:51:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+  public static void main(String args[]){
+    int no = 50;
+    long population = 7000000;
+    double salary = 4500.34;
+    float rate = 34.5f;
+
+    System.out.println("no = " + no);
+    System.out.println("population = " + population);
+    System.out.println("salary = " + salary);
+    System.out.println("rate = " + rate);
+  }
+}
